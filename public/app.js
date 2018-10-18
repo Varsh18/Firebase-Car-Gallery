a3f256eeaa556e6822bcb77710e493bfc02a3d03
varsh18 <varshiniravi1824@gmail.com>
2018-10-18 21:20:18 +0530

    Select collection

--- public/app.js
+++ public/app.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded",event=>{
   const app=firebase.app();
   const db=firebase.firestore();
   firebase.firestore().settings( { timestampsInSnapshots: true })
+   const carRef=db.collection('cars');
 });

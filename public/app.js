b6ceb0a35ec6ef2bc1e3faafc133b54c5e6b86cc
varsh18 <varshiniravi1824@gmail.com>
2018-10-18 21:28:43 +0530

    add table tag

--- public/app.js
+++ public/app.js
@@ -6,9 +6,11 @@ document.addEventListener("DOMContentLoaded",event=>{
   const query=carRef.orderBy('Model','asc')
   query.get()
         .then(product => {
+          document.write(`<table>`);
              product.forEach(doc => {
                 data=doc.data();
                 document.write(`<tr><td>${data.Model}</td> <td>${data.MPG}</td><td>${data.Cylinder}</td> <td>${data.Displacement}</td><td>${data.Horsepower}</td> <td>${data.Weight}</td><td>${data.Acceleration}</td> <td>${data.Year}</td><td>${data.Origin}</td> </tr>`);
              })
+            document.write(`</table>`);
         })
 });

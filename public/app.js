102a9c3a03400adb3cf1f489af28c9c1f6122876
varsh18 <varshiniravi1824@gmail.com>
2018-10-18 21:22:00 +0530

    Display Order by ascending

--- public/app.js
+++ public/app.js
@@ -3,4 +3,5 @@ document.addEventListener("DOMContentLoaded",event=>{
   const db=firebase.firestore();
   firebase.firestore().settings( { timestampsInSnapshots: true })
    const carRef=db.collection('cars');
+  const query=carRef.orderBy('Model','asc')
 });

2e296527b314b7cdb54cdbf90e0694b9e1d5ac72
varsh18 <varshiniravi1824@gmail.com>
2018-10-18 21:24:10 +0530

    Loop through each document

--- public/app.js
+++ public/app.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded",event=>{
   const query=carRef.orderBy('Model','asc')
   query.get()
         .then(product => {
-
+             product.forEach(doc => {
+             })
         })
 });
